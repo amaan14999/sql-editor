37d39988626d2b12f8dab63d6988512aa7da41e4
Amaan <amaan14999@gmail.com>
2023-12-11 19:34:31 +0530

    fix: remove million
    Signed-off-by: Amaan <amaan14999@gmail.com>

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,7 @@
 import { defineConfig } from "vite";
-import million from "million/compiler";
 import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [million.vite({ auto: true }), react()],
+  plugins: [react()],
 });
