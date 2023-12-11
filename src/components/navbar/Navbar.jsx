213143babfc9ec34989c7bdbf21d865c04e36c9d
Amaan <amaan14999@gmail.com>
2023-12-11 20:00:38 +0530

    feat: navbar style
    Signed-off-by: Amaan <amaan14999@gmail.com>

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -7,25 +7,34 @@ const Navbar = () => {
     <nav className="flex mx-8 py-2 px-4 font-bold gap-48 mt-4 rounded-lg bg-neutral-900 items-center justify-center">
       <NavLink
         to="/"
-        className="text-neutral-300 hover:text-neutral-100"
+        className="text-neutral-300 hover:text-cyan-400 duration-200 transition-colors"
         activeClassName="text-neutral-100"
       >
         Home
       </NavLink>
       <NavLink
         to="/editor"
-        className="text-neutral-300 hover:text-neutral-100"
+        className="text-neutral-300 hover:text-cyan-400 duration-200 transition-colors"
         activeClassName="text-neutral-100"
       >
         Editor
       </NavLink>
-      <a href="#" className="text-neutral-300 hover:text-neutral-100">
+      <a
+        href="#"
+        className="text-neutral-300 hover:text-cyan-400 duration-200 transition-colors"
+      >
         Login
       </a>
-      <a href="#" className="text-neutral-300 hover:text-neutral-100">
+      <a
+        href="#"
+        className="text-neutral-300 hover:text-cyan-400 duration-200 transition-colors"
+      >
         Contact
       </a>
-      <a href="" className="text-neutral-300 hover:text-neutral-100">
+      <a
+        href="https://github.com/amaan14999/atlan-sql-editor"
+        className="text-neutral-300 hover:text-cyan-400 duration-200 transition-colors"
+      >
         GitHub
       </a>
     </nav>
